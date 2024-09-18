b7ae2f468a08d4f72dd18a55a53d21111c22a728
abelvolpi <abelvolpi@gmail.com>
2024-09-18 16:24:31 +0100

    Two Sum

---
+++ src/main/kotlin/TwoSum.kt
@@ -0,0 +1,24 @@
+/*
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var intArray = IntArray(2)
+        for (i in 0 until nums.size) {
+            for (j in i + 1 until nums.size) {
+                val sum = nums[i] + nums[j]
+                if (sum == target) {
+                    intArray = intArrayOf(i, j)
+                }
+            }
+        }
+        return intArray
+    }
+}
+
+fun main(args: Array<String>) {
+    val response = TwoSum().twoSum(nums = intArrayOf(2,7,11,15), target = 26)
+    println(response.asList())
+}
