88e6fb505030c3b0d347f36156df87275ba6e4d7
abelvolpi <abelvolpi@gmail.com>
2025-12-15 23:08:05 -0300

    Convert the Temperature

---
+++ src/main/kotlin/ConvertTheTemperature.kt
@@ -0,0 +1,9 @@
+fun convertTemperature(celsius: Double): DoubleArray {
+    val kelvin = celsius + 273.15
+    val fahrenheit = celsius * 1.80 + 32.00
+    return doubleArrayOf(kelvin, fahrenheit)
+}
+
+fun main() {
+    println(convertTemperature(36.50))
+}
